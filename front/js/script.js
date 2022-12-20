71707583751dfa7c84150232b13733a82fb03af4
Florient Leonard <florient_leonard@hotmail.com>
2022-12-20 23:01:16 +0400

    Ajout dynamique d'un produit sur la page d'accueil

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,6 @@
+const items = document.getElementById("items")
+console.log(items)
+
 fetch("http://localhost:3000/api/products")
 .then(function(res) {
     if (res.ok) {
@@ -5,8 +8,9 @@ fetch("http://localhost:3000/api/products")
     }
 })
 .then(function(value) {
-    console.table(value[0]._id)
+    items.innerHTML = "<a href='.product.html?id" + value[0]._id + "'><article><img src='" + value[0].imageUrl + "' alt=" + value[0].altTxt + "><h3 class='productName'>" + value[0].name + "</h3><p class='productDescription'>" + value[0].description + "</p></article></a>"
 })
 .catch(function(err) {
     console.log("erreur")
 })
+console.log(items)
