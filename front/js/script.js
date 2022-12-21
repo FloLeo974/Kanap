ff97f116c409f5d5dffd8f75802816dd41276689
Florient Leonard <florient_leonard@hotmail.com>
2022-12-21 19:51:15 +0400

    Affichage de tous les produits avec for... in...

--- front/js/script.js
+++ front/js/script.js
@@ -1,5 +1,4 @@
 const items = document.getElementById("items")
-console.log(items)
 
 fetch("http://localhost:3000/api/products")
 .then(function(res) {
@@ -8,9 +7,30 @@ fetch("http://localhost:3000/api/products")
     }
 })
 .then(function(value) {
-    items.innerHTML = "<a href='.product.html?id" + value[0]._id + "'><article><img src='" + value[0].imageUrl + "' alt=" + value[0].altTxt + "><h3 class='productName'>" + value[0].name + "</h3><p class='productDescription'>" + value[0].description + "</p></article></a>"
+    for (let i in value){
+        let newLien = document.createElement('a')
+        newLien.setAttribute("href", "./product.html?id=" + value[i]._id)
+        items.appendChild(newLien)
+
+        let newArticle = document.createElement('article')
+        newLien.appendChild(newArticle)
+
+        let newImage = document.createElement('img')
+        newImage.setAttribute("src", value[i].imageUrl)
+        newImage.setAttribute("alt", value[i].altTxt)
+        newArticle.appendChild(newImage)
+
+        let newName = document.createElement('h3')
+        newName.classList.add("productName")
+        newName.innerHTML = value[i].name
+        newArticle.appendChild(newName)
+
+        let newDescription = document.createElement('p')
+        newDescription.classList.add("productDescription")
+        newDescription.innerText = value[i].description
+        newArticle.appendChild(newDescription)
+    }
 })
 .catch(function(err) {
     console.log("erreur")
 })
-console.log(items)
