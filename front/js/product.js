ec217a1e121a0a43f379b33e84bac50c205666de
Florient Leonard <florient_leonard@hotmail.com>
2022-12-23 10:57:41 +0400

    Ajout des informations du produit: à poursuivre

--- front/js/product.js
+++ front/js/product.js
@@ -2,6 +2,12 @@ const url = new URL(document.location.href)
 const searchParams = new URLSearchParams(url.search)
 const idProduct = searchParams.get('id')
 
+const imageProductClass = document.getElementsByClassName("item__img")
+const imageProduct = imageProductClass [0]
+const nameProduct = document.getElementById("title")
+const priceProduct = document.getElementById("price")
+const descriptionProduct = document.getElementById("description")
+
 fetch("http://localhost:3000/api/products/"+idProduct)
 .then(function(res) {
     if (res.ok) {
@@ -9,7 +15,14 @@ fetch("http://localhost:3000/api/products/"+idProduct)
     }
 })
 .then(function(value) {
-    console.log(value)
+    let newImage = document.createElement('img')
+    newImage.setAttribute("src", value.imageUrl)
+    newImage.setAttribute("alt", value.altTxt)
+    imageProduct.appendChild(newImage)
+
+    nameProduct.innerText = value.name
+    priceProduct.innerText = value.price
+    descriptionProduct.innerText = value.description
 })
 .catch(function(err) {
     console.log("erreur")
