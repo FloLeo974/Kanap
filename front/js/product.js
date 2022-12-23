b50155934e666c273f56c049084bc978b5f29e3b
Florient Leonard <florient_leonard@hotmail.com>
2022-12-23 17:22:16 +0400

    Ajout des informations du produit: couleurs

--- front/js/product.js
+++ front/js/product.js
@@ -9,6 +9,7 @@ const imageProduct = imageProductClass [0]
 const nameProduct = document.getElementById("title")
 const priceProduct = document.getElementById("price")
 const descriptionProduct = document.getElementById("description")
+const colorsProduct = document.getElementById("colors")
 
 // On récupère les informations du produit au niveau de l'API à partir de son id et on affiche les informations
 function afficherInformationsProduit () {
@@ -27,6 +28,14 @@ function afficherInformationsProduit () {
         nameProduct.innerText = value.name
         priceProduct.innerText = value.price
         descriptionProduct.innerText = value.description
+
+        for (let i in value.colors) {
+            let newOption = document.createElement('option')
+            newOption.setAttribute("value", value.colors[i])
+            newOption.innerText = value.colors[i]
+            colorsProduct.appendChild(newOption)
+            console.log(colorsProduct)
+        }
     })
     .catch(function(err) {
         console.log("erreur")
