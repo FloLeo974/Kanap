202fe538c2c9459fffa2d6e789f294753ad98b81
Florient Leonard <florient_leonard@hotmail.com>
2022-12-23 16:49:49 +0400

    Déclaration et appel de la fonction d'affichage

--- front/js/product.js
+++ front/js/product.js
@@ -1,29 +1,36 @@
+// On récupère l'id du produit depuis l'url
 const url = new URL(document.location.href)
 const searchParams = new URLSearchParams(url.search)
 const idProduct = searchParams.get('id')
 
+// On cible les éléments du DOM
 const imageProductClass = document.getElementsByClassName("item__img")
 const imageProduct = imageProductClass [0]
 const nameProduct = document.getElementById("title")
 const priceProduct = document.getElementById("price")
 const descriptionProduct = document.getElementById("description")
 
-fetch("http://localhost:3000/api/products/"+idProduct)
-.then(function(res) {
-    if (res.ok) {
-    return res.json()
-    }
-})
-.then(function(value) {
-    let newImage = document.createElement('img')
-    newImage.setAttribute("src", value.imageUrl)
-    newImage.setAttribute("alt", value.altTxt)
-    imageProduct.appendChild(newImage)
+// On récupère les informations du produit au niveau de l'API à partir de son id et on affiche les informations
+function afficherInformationsProduit () {
+    fetch("http://localhost:3000/api/products/"+idProduct)
+    .then(function(res) {
+        if (res.ok) {
+        return res.json()
+        }
+    })
+    .then(function(value) {
+        let newImage = document.createElement('img')
+        newImage.setAttribute("src", value.imageUrl)
+        newImage.setAttribute("alt", value.altTxt)
+        imageProduct.appendChild(newImage)
 
-    nameProduct.innerText = value.name
-    priceProduct.innerText = value.price
-    descriptionProduct.innerText = value.description
-})
-.catch(function(err) {
-    console.log("erreur")
-})
+        nameProduct.innerText = value.name
+        priceProduct.innerText = value.price
+        descriptionProduct.innerText = value.description
+    })
+    .catch(function(err) {
+        console.log("erreur")
+    })
+}
+
+afficherInformationsProduit ()
