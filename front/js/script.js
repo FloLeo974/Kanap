b3984d64cf807770eaf9a1d315f4d013529ded56
Florient Leonard <florient_leonard@hotmail.com>
2022-12-23 16:27:39 +0400

    Déclaration et appel de la fonction d'affichage

--- front/js/script.js
+++ front/js/script.js
@@ -1,36 +1,40 @@
 const items = document.getElementById("items")
 
-fetch("http://localhost:3000/api/products")
-.then(function(res) {
-    if (res.ok) {
-    return res.json()
-    }
-})
-.then(function(value) {
-    for (let i in value){
-        let newLien = document.createElement('a')
-        newLien.setAttribute("href", "./product.html?id=" + value[i]._id)
-        items.appendChild(newLien)
+function afficherProduits () {
+    fetch("http://localhost:3000/api/products")
+    .then(function(res) {
+        if (res.ok) {
+        return res.json()
+        }
+    })
+    .then(function(value) {
+        for (let i in value){
+            let newLien = document.createElement('a')
+            newLien.setAttribute("href", "./product.html?id=" + value[i]._id)
+            items.appendChild(newLien)
 
-        let newArticle = document.createElement('article')
-        newLien.appendChild(newArticle)
+            let newArticle = document.createElement('article')
+            newLien.appendChild(newArticle)
 
-        let newImage = document.createElement('img')
-        newImage.setAttribute("src", value[i].imageUrl)
-        newImage.setAttribute("alt", value[i].altTxt)
-        newArticle.appendChild(newImage)
+            let newImage = document.createElement('img')
+            newImage.setAttribute("src", value[i].imageUrl)
+            newImage.setAttribute("alt", value[i].altTxt)
+            newArticle.appendChild(newImage)
 
-        let newName = document.createElement('h3')
-        newName.classList.add("productName")
-        newName.innerHTML = value[i].name
-        newArticle.appendChild(newName)
+            let newName = document.createElement('h3')
+            newName.classList.add("productName")
+            newName.innerHTML = value[i].name
+            newArticle.appendChild(newName)
 
-        let newDescription = document.createElement('p')
-        newDescription.classList.add("productDescription")
-        newDescription.innerText = value[i].description
-        newArticle.appendChild(newDescription)
-    }
-})
-.catch(function(err) {
-    console.log("erreur")
-})
+            let newDescription = document.createElement('p')
+            newDescription.classList.add("productDescription")
+            newDescription.innerText = value[i].description
+            newArticle.appendChild(newDescription)
+        }
+    })
+    .catch(function(err) {
+        console.log("erreur")
+    })
+}
+
+afficherProduits ()
