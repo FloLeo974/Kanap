400a6f753f3d4defb0c90bb3d077cd5b7c4ce792
Florient Leonard <florient_leonard@hotmail.com>
2022-12-23 10:11:03 +0400

    Récupération des informations du produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,16 @@
 const url = new URL(document.location.href)
 const searchParams = new URLSearchParams(url.search)
 const idProduct = searchParams.get('id')
+
+fetch("http://localhost:3000/api/products/"+idProduct)
+.then(function(res) {
+    if (res.ok) {
+    return res.json()
+    }
+})
+.then(function(value) {
+    console.log(value)
+})
+.catch(function(err) {
+    console.log("erreur")
+})
