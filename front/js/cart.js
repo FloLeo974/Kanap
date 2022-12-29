62901b23fbbf70d95abb49f5b1c418d8b392d639
Florient Leonard <florient_leonard@hotmail.com>
2022-12-29 11:59:56 +0400

    Modification dénomination variables

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,3 +1,3 @@
-let contenuAjoutLinea = localStorage.getItem("obj"); // récupération des données stockées (pour tester mais sinon dans cart.js)
-let contenuAjoutJson = JSON.parse(contenuAjoutLinea); // mis en format javascript
-console.table(contenuAjoutJson) // affichage test dans la console du contenu du panier
+let contenuPanierLinea = localStorage.getItem("obj"); // récupération des données stockées
+let contenuPanierJson = JSON.parse(contenuPanierLinea); // reformation de l'objet
+console.table(contenuPanierJson) // affichage test dans la console du contenu du panier
--- front/js/product.js
+++ front/js/product.js
@@ -54,12 +54,12 @@ recupererInformationsProduit ()
 /* GESTION DU PANIER: */
 // On récupére l'array qui contient les produits ajoutés au panier 
 // Ou on le crée s'il n'existe pas encore dans le local storage
-let contenuAjout = JSON.parse(localStorage.getItem("obj")) || []
+let contenuPanier = JSON.parse(localStorage.getItem("obj")) || []
 
 // On crée une fonction pour mémoriser le contenu du panier dans le local storage
 function memoriserPanier() {
-    let contenuAjoutLinea = JSON.stringify(contenuAjout) // mis en format json
-    localStorage.setItem("obj", contenuAjoutLinea) // stockage dans le local storage
+    let contenuPanierLinea = JSON.stringify(contenuPanier) // linéarisation de l'objet
+    localStorage.setItem("obj", contenuPanierLinea) // stockage dans le local storage
 }
 
 // On gère le remplissage du panier
@@ -71,12 +71,12 @@ function remplirPanier () {
     }
     else { // si le produit n'est pas encore dans le panier on l'ajoute sinon on met seulement à jour la quantité
         let existingProduct = (element) => element.id == idProduct && element.color == colorProductSelected // recherche du produit dans l'array
-        let existingProductIndex = contenuAjout.findIndex(existingProduct) // indique l'index du produit s'il existe sinon "-1"
+        let existingProductIndex = contenuPanier.findIndex(existingProduct) // indique l'index du produit s'il existe sinon "-1"
         if (existingProductIndex == -1) { // si le produit n'est pas encore présent dans le panier
-        contenuAjout.push({ id: idProduct, quantity: quantityProduct, color: colorProductSelected })// ajout d'une ligne dans l'array avec le contenu ajouté
+        contenuPanier.push({ id: idProduct, quantity: quantityProduct, color: colorProductSelected })// ajout d'une ligne dans l'array avec le contenu ajouté
         }
         else { // si le produit est déjà dans le panier
-            contenuAjout[existingProductIndex].quantity += quantityProduct // addition de la nouvelle quantité à la précédente
+            contenuPanier[existingProductIndex].quantity += quantityProduct // addition de la nouvelle quantité à la précédente
         }
         alert("Votre produit a été ajouté au panier") // message de succés pour l'utilisateur
     }
@@ -88,7 +88,7 @@ boutonAjouter.addEventListener('click', function(){ // écoute du clic sur le bo
     remplirPanier() // activation de la fonction de remplissage du panier
 
     // A RETIRER UNE FOIS TERMINE: JUSTE POUR VOIR LE RESULTAT DANS LA CONSOLE:
-    let contenuAjoutLinea = localStorage.getItem("obj"); // récupération des données stockées (pour tester mais sinon dans cart.js)
-    let contenuAjoutJson = JSON.parse(contenuAjoutLinea); // mis en format javascript
-    console.table(contenuAjoutJson) // affichage test dans la console du contenu du panier
+    let contenuPanierLinea = localStorage.getItem("obj"); // récupération des données stockées (pour tester mais sinon dans cart.js)
+    let contenuPanierJson = JSON.parse(contenuPanierLinea); // mis en format javascript
+    console.table(contenuPanierJson) // affichage test dans la console du contenu du panier
 })
